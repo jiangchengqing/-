d6b0527aebf6679fce662ac12b83af09b06ca960
姜呈擎 <1119154811@qq.com>
2020-07-12 14:56:51 +0800

    添加项目文件。

---
+++ 匿名方法/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 匿名方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate md = () => {  Console.WriteLine("lambad表达式");  };
+            md();
+            Console.ReadKey();
+        }
+    }
+
+    public delegate void MyDelegate();
+}
